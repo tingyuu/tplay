dbba2b5adffaceb216886efce0b22f9fe58e9a56
tingyuu <389625819@qq.com>
2017-12-22 15:50:45 +0800

    no message

--- application/admin/common.php
+++ application/admin/common.php
@@ -17,7 +17,7 @@
  * @param $password_code 密码额外加密字符
  * @return string
  */
-function password($password, $password_code='pengwei')
+function password($password, $password_code='lshi4AsSUrUOwWV')
 {
     return md5(md5($password) . md5($password_code));
 }
